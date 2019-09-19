743d9d7932ccd2cc45bfa512ddd9273e2e631052
Javier <fcogzz.r@gmail.com>
2019-09-18 19:08:22 -0500

    [feature]: show error if exists

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { Form, Input, Button } from 'antd';
+import { Alert } from 'antd';
 
 const Question = () => {
 
@@ -10,18 +11,23 @@ const Question = () => {
   // validate budget
   const addBudget = e => {
     e.preventDefault();
+    console.log(e)
     if(amount < 1 || isNaN(amount)){
       setError(true);
       return;
     }
+    setError(false);
 
     // send budget to main component
 
-  }
+  };
 
   return (
     <Fragment>
       <h2>What is your budget?</h2>
+
+      { error ? <Alert message="The budget is wrong" type="error" style={{width: 200, marginBottom: 12 }} /> : null }
+
       <Form layout="vertical" onSubmit={addBudget}>
         <Form.Item label="Enter the amount you would like to spend each week">
           <Input 
@@ -30,7 +36,7 @@ const Question = () => {
             style={{width: 200}}/>
         </Form.Item>
         <Form.Item>
-          <Button type="primary">Save</Button>
+          <Button type="primary" htmlType="submit">Save</Button>
         </Form.Item>
       </Form>
     </Fragment>
